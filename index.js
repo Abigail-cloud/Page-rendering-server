793cc63a1f6ee60a8f833c0b84a5f43e9f75eec4
Toluwalase Abigail <toluabby12@gmail.com>
2022-05-20 10:51:11 +0100

    Page Render

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+
+//The main Page
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/index.html'));
+    //__dirname : Will resolve to your project folder.
+});
+
+
+// <a href="about.html"></a>
+
+router.get('/about.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/about.html'));
+});
+
+router.get('/contact.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/contact.html'));
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 8080);
+
+console.log('Running at Port 8080');
